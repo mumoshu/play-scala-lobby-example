54b13207bcf63638047817190ce0030380923979
KUOKA Yusuke <kuoka@furyu.jp>
2011-06-21 11:28:08 +0900

    Reformatted java code.

--- app/controllers/RoomsSocket.java
+++ app/controllers/RoomsSocket.java
@@ -2,7 +2,9 @@ package controllers;
 
 import play.Logger;
 import play.libs.F;
+
 import static play.mvc.Http.WebSocketEvent.TextFrame;
+
 import play.mvc.Http;
 import play.mvc.WebSocketController;
 import models.Room;
@@ -23,39 +25,39 @@ public class RoomsSocket extends WebSocketController {
         }
         F.EventStream<Event> events = room.events().eventStream();
 
-      while (inbound.isOpen()) {
-        play.libs.F.Either<Http.WebSocketEvent, Event> e = await(F.Promise.waitEither(
-                inbound.nextEvent(),
-                events.nextEvent()
-        ));
+        while (inbound.isOpen()) {
+            play.libs.F.Either<Http.WebSocketEvent, Event> e = await(F.Promise.waitEither(
+                    inbound.nextEvent(),
+                    events.nextEvent()
+            ));
 
-        for (String message : TextFrame.match(e._1)) {
-            Logger.info("Received a message: %s", message);
+            for (String message : TextFrame.match(e._1)) {
+                Logger.info("Received a message: %s", message);
 
-            String[] methodAndArguments = message.split(":");
-            String method = methodAndArguments[0];
-            String[] args = new String[]{};
-            if (methodAndArguments.length > 1) {
-                args = Arrays.copyOfRange(methodAndArguments, 1, methodAndArguments.length);
-            }
-            List<String> arguments = Arrays.asList(args);
-            Logger.info("%s, %s", method, arguments);
-
-            if (method.equals("join")) {
-                room.join(user);
-            } else if (method.equals("say")) {
-                String what = null;
-                if (arguments.size() > 0) {
-                    what = arguments.get(0);
-                    room.say(user, what);
+                String[] methodAndArguments = message.split(":");
+                String method = methodAndArguments[0];
+                String[] args = new String[]{};
+                if (methodAndArguments.length > 1) {
+                    args = Arrays.copyOfRange(methodAndArguments, 1, methodAndArguments.length);
                 }
-                if (what == null) {
-                    Logger.warn("Nothing to say!");
+                List<String> arguments = Arrays.asList(args);
+                Logger.info("%s, %s", method, arguments);
+
+                if (method.equals("join")) {
+                    room.join(user);
+                } else if (method.equals("say")) {
+                    String what = null;
+                    if (arguments.size() > 0) {
+                        what = arguments.get(0);
+                        room.say(user, what);
+                    }
+                    if (what == null) {
+                        Logger.warn("Nothing to say!");
+                    }
                 }
             }
+
+            Logger.info(e.toString());
         }
-
-        Logger.info(e.toString());
-      }
     }
 }
