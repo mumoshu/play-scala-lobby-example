67cea7faec8028adb3993ae9c1081b51eede2141
KUOKA Yusuke <ykuoka@gmail.com>
2011-06-24 21:16:22 -0700

    User registration pages.

---
+++ app/controllers/Users.scala
@@ -0,0 +1,34 @@
+package controllers
+
+import play.mvc._
+import models.User
+import play.cache.Cache
+import play.mvc.Scope.Session
+import play.data.validation.Validation
+
+object Users extends Controller {
+  import views.Users.html
+
+  def join() = html.join()
+
+  def create() = {
+    val name = params.get("name")
+    val password = params.get("password")
+    val email = params.get("email")
+
+    Validation.required("name", name).message("Name is required.")
+    Validation.required("email", email)
+    Validation.required("password", password)
+    Validation.email("email", email).message("Malformed email address.")
+
+    if (Validation.hasErrors) {
+      join()
+    } else {
+      val user = User.join(name, password, email)
+      Cache.add(session.getId + "-user", user)
+      created(user)
+    }
+  }
+
+  def created(user: User) = html.created(user)
+}
--- test/LobbyTest.scala
+++ test/LobbyTest.scala
@@ -1,6 +1,24 @@
+import java.io.File
 import org.scalatest.matchers.ShouldMatchers
 import play.test.{FunctionalFlatSpec, FunctionalTest}
+import collection.JavaConversions._
 
 class LobbyTest extends FunctionalFlatSpec with ShouldMatchers {
-//  "Lobby" should accept
+  it should "show the user registration page" in {
+    val response = GET("/users/join")
+    response shouldBeOk()
+    getContent(response) should not include ("Error")
+  }
+
+  it should "create a new user" in {
+    val parameters = Map(
+      "name" -> "_name_",
+      "password" -> "_password_",
+      "email" -> "_email_"
+    )
+    val response = POST("/users/create", parameters, Map[String, File]())
+    response shouldBeOk()
+    // How do you test redirections from POST requets?
+//    response headerShouldBe("Location", "/users/created")
+  }
 }
